ebda0ccaef96eb3fd61e0b3e3e09e2a3fbd364c1
harut0111 <hmuqoyan@gmail.com>
2019-11-08 16:07:49 +0400

    cm

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Steper from './components/Steper'
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+          <Steper/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Steper/index.tsx
@@ -0,0 +1,95 @@
+// import {FormControl, FormHelperText, Input, InputLabel} from "@material-ui/core";
+import Button from '@material-ui/core/Button';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Stepper from '@material-ui/core/Stepper';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import * as React from 'react';
+import ContactInfo from '../ContactInfo';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        backButton: {
+            marginRight: theme.spacing(1),
+        },
+        instructions: {
+            marginBottom: theme.spacing(1),
+            marginTop: theme.spacing(1),
+        },
+        root: {
+            width: '90%',
+        }
+    }),
+);
+
+function getSteps() {
+    return ['Контактная информация', 'Информация о грузе', 'Отправка'];
+}
+
+function getStepContent(stepIndex: number){
+    switch (stepIndex) {
+        case 0:
+            return <ContactInfo />;
+        case 1:
+            return 'Информация о грузе';
+        case 2:
+            return 'Отправка';
+        default:
+            return 'Unknown stepIndex';
+    }
+}
+
+export default function HorizontalLabelPositionBelowStepper() {
+    const classes = useStyles();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const steps = getSteps();
+
+    const handleNext = () => {
+        setActiveStep(prevActiveStep => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep(prevActiveStep => prevActiveStep - 1);
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+    };
+
+    return (
+        <div className={classes.root}>
+            <Stepper activeStep={activeStep} alternativeLabel={true}>
+                {steps.map(label => (
+                    <Step key={label}>
+                        <StepLabel>{label}</StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
+            <div>
+                {activeStep === steps.length ? (
+                    <div>
+                        <Typography className={classes.instructions}>All steps completed</Typography>
+                        <Button onClick={handleReset}>Reset</Button>
+                    </div>
+                ) : (
+                    <div>
+                        <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+                        <div>
+                            <Button
+                                disabled={activeStep === 0}
+                                onClick={handleBack}
+                                className={classes.backButton}
+                            >
+                                Back
+                            </Button>
+                            <Button variant="contained" color="primary" onClick={handleNext}>
+                                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                            </Button>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
